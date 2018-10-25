6d6a28fb96c22ce1f07b55a8ba40a0e30bf1d455
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-10-25 17:33:36 +0200

    7

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -12,6 +12,26 @@ public class Student {
         this.prezimeStudenta = prezime;
         this.brojIndeksa = indeks;
     }
+    //getteri
+    public String getIme() {
+        return imeStudenta;
+    }
+    public String getPrezime() {
+        return prezimeStudenta;
+    }
+    public int getIndeks() {
+        return brojIndeksa;
+    }
 
+    //setteri
+    public void setIme(String ime) {
+        this.imeStudenta = ime;
+    }
+    public void setPrezime(String prezime) {
+        this.prezimeStudenta = prezime;
+    }
+    public void setIndeks(int index) {
+        this.brojIndeksa = index;
+    }
 }
 
