3b07dd4e10de1ad71263e4cc36184add306b369d
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-10-25 17:30:55 +0200

    3

--- src/com/company/Predmet.java
+++ src/com/company/Predmet.java
@@ -16,4 +16,50 @@ public class Predmet {
         studenti = new Student[0];
     }
 
+    //dodavanje novog studenta na predmet, poziv je u metodi upisi
+    public void DodajNovogStudenta(Student s) {
+        Student[] noviStudenti = new Student[getStudenti().length+1];
+        if (getStudenti().length < maxBrojStudenata) {
+            noviStudenti = new Student[getStudenti().length +1];
+            for (int i = 0; i < getStudenti().length; i++) {
+                noviStudenti[i] = getStudenti()[i];
+            }
+            noviStudenti[getStudenti().length] = s;
+        }
+        setStudenti(noviStudenti);
+    }
+
+    //upisivanje studenta na predmet
+    public void upisi(Student student) throws IllegalArgumentException {
+        if (studenti.length > maxBrojStudenata)
+            throw new IllegalArgumentException ("Predmet popunjen.");
+        else
+            DodajNovogStudenta(student);
+    }
+
+    //getteri
+    public String getNazivPredmeta() {
+        return nazivPredmeta;
+    }
+    public int getSifraPredmeta() {
+        return sifraPredmeta;
+    }
+    public int getMaxBrojStudenata() {
+        return maxBrojStudenata;
+    }
+    public Student[] getStudenti() {
+        return  studenti;
+    }
+
+    //setteri
+    public void setSifraPredmeta(int novaSifra) {
+        sifraPredmeta = novaSifra;
+    }
+    public void setNazivPredmeta(String noviNaziv) {
+        nazivPredmeta = noviNaziv;
+    }
+    public void setStudenti(Student[] studenti) {
+        this.studenti = studenti;
+    }
+
 }
