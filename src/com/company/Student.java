e8cf6a97b293435afa31192e55be91c4e3672c70
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-10-25 17:33:13 +0200

    6

---
+++ src/com/company/Student.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Student {
+    //atrubuti
+    private String imeStudenta;
+    private String prezimeStudenta;
+    private int brojIndeksa;
+
+    //konstruktor
+    public Student(String ime, String prezime, int indeks) {
+        this.imeStudenta = ime;
+        this.prezimeStudenta = prezime;
+        this.brojIndeksa = indeks;
+    }
+
+}
+
